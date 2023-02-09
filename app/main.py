e222863219943a6408b6875cec5fd9b8b5f7023e
Xejb <vladiq-artamonenko@rambler.ru>
2023-02-09 16:35:41 +0300

    Query

--- app/main.py
+++ app/main.py
@@ -1,24 +1,31 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Query
 from schemas import Book
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
-    return {"key": "func home"}
-
-
-@app.get('/{pk}')
-def get_item(pk: int, q: str = None):
-    return {'key': pk, "q": q}
-
-
-@app.get('/user/{pk}/items/{item}')
-def get_user_item(pk: int, item: str):
-    return {'user': pk, 'item': item}
+# @app.get('/')
+# def home():
+#     return {"key": "func home"}
+#
+#
+# @app.get('/{pk}')
+# def get_item(pk: int, q: str = None):
+#     return {'key': pk, "q": q}
+#
+#
+# @app.get('/user/{pk}/items/{item}')
+# def get_user_item(pk: int, item: str):
+#     return {'user': pk, 'item': item}
 
 
 @app.post('/book')
 def create_book(item: Book):
     return item
+
+
+@app.get('/book')
+def get_book(q: List[str] = Query(['test', 'test2'], description='Search bks', regex='^', deprecated=True)):
+    return q
